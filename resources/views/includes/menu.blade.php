778138b477aad70ed6579d2f5caa1b963a683459
davidhalberstad <54964061+davidhalberstad@users.noreply.github.com>
2020-03-18 21:50:20 -0300

    le agregue chequeos de usuarios al menu

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,6 +1,8 @@
+@if(auth()->check())
 <nav class="mt-2">
   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     <!-- Panel de Control del Calificador -->
+    @if(auth()->user()->is_admin)
     <li class="nav-item has-treeview menu-open">
       <a href="#" class="nav-link active">
         <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -17,7 +19,6 @@
           </a>
         </li>
 
-
         <li class="nav-item">
           <a href="{{ url('denuncias') }}" class="nav-link {{ ! Route::is('denuncias') ?: 'active' }}">
             <i class="far fa-circle nav-icon"></i>
@@ -27,6 +28,8 @@
 
       </ul>
     </li>
+    @endif
 
   </ul>
 </nav>
+@endif
