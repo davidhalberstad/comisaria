a818d16d7abc117652b36df850068072b44f8993
davidhalberstad <54964061+davidhalberstad@users.noreply.github.com>
2020-04-02 09:44:11 -0300

    Se creo el Modelo

---
+++ app/ModusOperandy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModusOperandy extends Model
+{
+      protected $table = 'modus_operandi';
+
+      protected $fillable = [
+        'id', 'modus_operandi',
+      ];
+
+}
