dcf4c1fbe402d88272c0ce6be6d71547a51fd523
davidhalberstad <54964061+davidhalberstad@users.noreply.github.com>
2020-04-02 09:42:29 -0300

    Se creo el Modelo

---
+++ app/Hecho.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hecho extends Model
+{
+      protected $table = 'tipos_delitos';
+
+      protected $fillable = [
+        'delito', 'id_delitos_titulos', 'cod', 'tipo_delito', 'id',
+      ];
+
+}
