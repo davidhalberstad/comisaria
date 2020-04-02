0c89e302ca115e98f3098ca366229348f5f55e0e
davidhalberstad <54964061+davidhalberstad@users.noreply.github.com>
2020-04-02 09:42:18 -0300

    Se creo el Modelo

---
+++ app/Circunscripcion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Circunscripcion extends Model
+{
+      protected $table = 'select_1';
+
+      protected $fillable = [
+          'id', 'opcion'
+      ];
+
+}
