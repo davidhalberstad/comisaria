8ae0e1f62fdd773994ad62fde3c9f749c8ef7d69
davidhalberstad <54964061+davidhalberstad@users.noreply.github.com>
2020-04-06 12:54:50 -0300

    Creo el Modelo de VehiculoRobado

---
+++ app/VehiculoRobado.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VehiculoRobado extends Model
+{
+      protected $table = 'vehiculos_robados';
+
+      protected $fillable = [
+                            'ur',
+                            'depe',
+                            'fecha date',
+                            'dni',
+                            'apeynom',
+                            'caracteristicas_vehiculo',
+                            'habido',
+                            'fecha_ubicacion',
+                            'localidad',
+                            'lugar_habido',
+                            'datos_vehiculo',
+                            'tipo_rodado',
+                            'dominio',
+                            'chasis',
+                            'motor',
+                            'nro integer',
+                            'anio integer',
+                            'cod'
+      ];
+
+}
